d96f4f4829b3b1cd99a7e511ffdc5f739c5bfa8a
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-04 16:03:12 -0600

    Add turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'oauth#callback', as: 'oauth_callback'
+  get '/auth/failure', to: 'oauth#failure', as: 'oauth_failure'
 end
